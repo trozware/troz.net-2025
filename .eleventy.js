951b6e39aab3341e049b45a9570839ff9087f63d
Sarah Reichelt <sarah@troz.net>
2025-02-26 11:30:45 +1000

    scss, dark mode toggle

--- .eleventy.js
+++ .eleventy.js
@@ -9,13 +9,13 @@ import { getPosts, getRecentPosts, getAllUniqueCategories, getCategoriesByName }
 
 export default async function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/images")
-	eleventyConfig.addPassthroughCopy("./src/styles")
+	eleventyConfig.addPassthroughCopy("./src/styles/contact.css")
 	eleventyConfig.addPassthroughCopy("./src/books/*_sample.html")
 	eleventyConfig.addPassthroughCopy("./src/favicon.ico")
 	eleventyConfig.addPassthroughCopy("./src/icon.svg")
 	eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png")
 	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
-
+	eleventyConfig.addPassthroughCopy({ 'src/js/theme_switcher.js': '/js/theme_switcher.js' });
 	
 	eleventyConfig.addFilter("longDate", longDate)
 	eleventyConfig.addFilter("shortDate", shortDate)
